01235d350b208417e77a0b5f77ec54ebacf29204
Davecamerini <camerini.davide@gmail.com>
2024-11-12 10:42:04 +0100

    changes

--- React/aind/src/App.tsx
+++ React/aind/src/App.tsx
@@ -8,21 +8,10 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-        <title>AIND</title>
+        <title>AIND - Art Is Not Dead</title>
       </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to fuck your mom.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <h1>Welcome to AIND</h1>
       </header>
       <body className='mainContent'>
