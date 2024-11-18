5c2ef4519df868164d88c702a6dfaf93fd83278e
Davecamerini <camerini.davide@gmail.com>
2024-11-18 10:38:25 +0100

    test

--- React/react-calendar-wp-plugin/src/App.tsx
+++ React/react-calendar-wp-plugin/src/App.tsx
@@ -1,13 +1,24 @@
+// eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
+import { infinity } from 'ldrs';
 
 function App() {
+  infinity.register();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <l-infinity
+          size="250"
+          stroke="15"
+          stroke-length="0.1"
+          bg-opacity="0.1"
+          speed="1.5"
+          color="white" 
+        ></l-infinity>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
