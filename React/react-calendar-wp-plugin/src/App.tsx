e6d5ecd7f8b0000da3dd07b5d9c081b01ee5bb4f
Davecamerini <camerini.davide@gmail.com>
2024-11-20 20:30:15 +0100

    redo the whole thing

--- React/react-calendar-wp-plugin/src/App.tsx
+++ React/react-calendar-wp-plugin/src/App.tsx
@@ -1,52 +1,23 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Calendar from './components/react-calendar/Calendar.tsx';
 
 function App() {
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const [events, setEvents] = useState<CalendarEvent[]>();
-
-  interface CalendarEvent {
-    id: number;
-    name: string;
-    date: Date;
-    link: string;
-  }
-
-  interface CustomEvent {
-    id: string | number;
-    name: string;
-    date: Date;
-    link: string;
-  }
-
   return (
-    <div className="wp-react-calendar">
-      <Calendar 
-        currentDate={currentDate}
-        onNavigate={(date: Date) => setCurrentDate(date)}
-        events={events as CalendarEvent[]}
-        onEventAdd={(event: CustomEvent) => {
-          const calendarEvent: CalendarEvent = {
-            id: Number(event.id),
-            name: event.name,
-            date: event.date,
-            link: event.link,
-          };
-          setEvents(prev => [...(prev || []), calendarEvent]);
-        }}
-        onEventEdit={(event: CustomEvent) => {
-          const calendarEvent: CalendarEvent = {
-            id: Number(event.id),
-            name: event.name,
-            date: event.date,
-            link: event.link,
-          };
-          setEvents(prev => 
-            prev?.map(e => e.id === calendarEvent.id ? calendarEvent : e)
-          );
-        }}
-      />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
