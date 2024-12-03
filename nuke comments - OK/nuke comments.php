66c36b4b783d1054516dfb707490209901f78a04
Davecamerini <camerini.davide@gmail.com>
2024-12-03 10:13:55 +0100

    name change

---
+++ nuke comments - OK/nuke comments.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Nuke Comments
+ * Description: A simple plugin to nuke the wp_comments table.
+ * Version: 1.0
+ * Author: <a href="https://www.davecamerini.it">Davecamerini</a>
+ */
+
+// Hook to add a menu item in the admin dashboard
+add_action('admin_menu', 'empty_comments_menu');
+
+function empty_comments_menu() {
+    add_menu_page('Nuke Comments', 'Nuke Comments', 'manage_options', 'nuke-comments', 'nuke_comments_page');
+}
+
+function nuke_comments_page() {
+    if (isset($_POST['nuke_comments'])) {
+        global $wpdb;
+        $wpdb->query("TRUNCATE TABLE $wpdb->comments");
+        echo '<div class="updated"><p>All comments have been nuked.</p></div>';
+    }
+    ?>
+    <div class="wrap">
+        <h1>Nuke Comments</h1>
+        <form method="post">
+            <input type="submit" name="nuke_comments" class="button button-primary" value="Nuke Comments" onclick="return confirm('Are you sure you want to delete all comments?');" />
+        </form>
+    </div>
+    <?php
+}
