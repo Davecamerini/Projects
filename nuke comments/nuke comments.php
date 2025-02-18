b0f9e0f464fcda3b033e7b0ed94149d9060ad6cf
Davecamerini <camerini.davide@gmail.com>
2025-02-18 11:33:41 +0100

    tidy

--- nuke comments/nuke comments.php
+++ nuke comments/nuke comments.php
@@ -15,14 +15,21 @@ function empty_comments_menu() {
 }
 
 function nuke_comments_page() {
+    global $wpdb;
+
+    // Get the total number of comments
+    $total_comments = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->comments");
+
     if (isset($_POST['nuke_comments'])) {
-        global $wpdb;
         $wpdb->query("TRUNCATE TABLE $wpdb->comments");
         echo '<div class="updated"><p>All comments have been nuked.</p></div>';
+        // Refresh the total number of comments after nuking
+        $total_comments = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->comments"); // Re-query the total comments
     }
     ?>
     <div class="wrap">
         <h1>Nuke Comments</h1>
+        <p>Total Comments: <strong><?php echo esc_html($total_comments); ?></strong></p>
         <form method="post" style="margin-top: 20px;">
             <input type="submit" name="nuke_comments" class="button button-primary" value="Nuke Comments" onclick="return confirm('Are you sure you want to delete all comments?');" />
         </form>
