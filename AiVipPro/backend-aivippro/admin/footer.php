7e7d6ca01e9920bfcd367b960bc05552b7fc02c9
Davecamerini <camerini.davide@gmail.com>
2025-04-14 13:45:32 +0200

    order

--- AiVipPro/backend-aivippro/admin/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer" style="background-color: #343a40;color: #ffffff;padding: 0px;text-align: center;position: sticky;bottom: 0;width: 100%;">
-        <div class="container">
-            <p>&copy; <?php echo date("Y"); ?> Lucia Ilaria Seglie</p>
-        </div>
-    </footer>
--- AiVipPro/backend-aivippro/partial/footer.php
+++
@@ -1,2 +0,0 @@
-
-<div class="copyright"><p>© <?php echo date('Y'); ?> | All Right Reserved. Zoi Yoga.</p></div>
